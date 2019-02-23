5ec32ce6b7e2e07a369e8fe050c5501411145e18
egilltor17 <egilltor17@ru.is>
2019-02-23 13:45:34 +0000

    Testing now operational

--- src/logic/main.js
+++ src/logic/main.js
@@ -4,19 +4,18 @@ function add(a, b) {
     return a + b;
 }
 
-function testClick(rowNr, colNr) {
-    var row = "row" + toString(rowNr);
-    var col = "col" + toString(colNr);
-    document.getElementsByClassName(row)
-    // test[0].innerText = "Test";
-    // for(var i = 0; i < test.length; i++) {
-    //     console.log(test[i]);
-    //     test[i].style.font-size = "2rem";
-    // }
-
+function testClick(boxNr, cellNr) {
+    var cell = "cell" + boxNr + cellNr;
+    console.log(cell);
+    document.getElementById(cell).style.backgroundColor = "black";
 }
 
-module.exports = {
-    add,
-    testClick,
-} 
+
+if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { 
+    module.exports = {
+        add,
+        testClick,
+    } 
+} else {
+    // window.testClick = testClick;
+}
