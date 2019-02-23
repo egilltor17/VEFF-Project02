874672e8bf16c8cc64cf1c6e805a925f0d2c0145
Oskar Pilkington <ohp9800@gmail.com>
2019-02-23 15:34:11 +0000

    send ajax now returns response

--- src/logic/main.js
+++ src/logic/main.js
@@ -16,18 +16,26 @@ function sendAjaxRequest(){
     //The URL to which we will send the request
     var url = 'https://veff213-sudoku.herokuapp.com/api/v1/sudoku/';
 
+    var res;
+
     //Perform an AJAX POST request to the url, and set the param 'myParam' in the request body to paramValue
     axios.post(url, { difficulty: paramValue })
         .then(function (response) {
             //When successful, print 'Success: ' and the received data
+            res = response;
             console.log("Success: ", response.data);
+            console.log(response);
+            
         })
         .catch(function (error) {
             //When unsuccessful, print the error.
             console.log(error);
+            
         })
         .then(function () {
             // This code is always executed, independent of whether the request succeeds or fails.
+             console.log(res.status);
+             return res;
         });
 }
 
