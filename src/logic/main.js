70e20339555610ecd0e52db31f49f30cec33efda
Oskar Pilkington <ohp9800@gmail.com>
2019-02-23 14:36:22 +0000

    added form to sudoku.html

--- src/logic/main.js
+++ src/logic/main.js
@@ -19,3 +19,26 @@ if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') {
 } else {
     // window.testClick = testClick;
 }
+
+
+function sendAjaxRequest(){
+    //Prepare the parameter value for 'myParam'
+    var paramValue = getElementById("difficulty").value;
+
+    //The URL to which we will send the request
+    var url = 'http://someinvaliddomain.is';
+
+    //Perform an AJAX POST request to the url, and set the param 'myParam' in the request body to paramValue
+    axios.post(url, { myParam: paramValue })
+        .then(function (response) {
+            //When successful, print 'Success: ' and the received data
+            console.log("Success: ", response.data);
+        })
+        .catch(function (error) {
+            //When unsuccessful, print the error.
+            console.log(error);
+        })
+        .then(function () {
+            // This code is always executed, independent of whether the request succeeds or fails.
+        });
+}
